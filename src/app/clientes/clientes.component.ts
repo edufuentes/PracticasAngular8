57ac125e248674f3ecafec4cd455aaa200004ff3
ER <edufuentes2323@gmail.com>
2019-12-04 16:29:58 -0300

    Creando archivo clientes.json.ts con la lista commit anterior
    se desacoplo la lista de clientes a un archivo .json.ts  que se coloco como una constante y luego se cargo en el metodo de inicializacion ngOnInit() del componente ClientesComponent

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,25 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
-export class ClientesComponent  {
+export class ClientesComponent  implements OnInit {
+  
 
-  clientes: Cliente[] = [
-            {id:1,nombre:'eduardo',apellido:'Rondón',email:'edu@gmail.com',createAt:'2019-12-04'},
-            {id:2,nombre:'jesus',apellido:'Fuentes',email:'jesus@gmail.com',createAt:'2019-12-03'},
-            {id:3,nombre:'Pedro',apellido:'Capriles',email:'pedro@gmail.com',createAt:'2019-12-02'},
-            {id:4,nombre:'Oscar',apellido:'Corvalan',email:'oscar@gmail.com',createAt:'2019-12-11'},
-            {id:5,nombre:'Hugo',apellido:'Rojas',email:'hugo@gmail.com',createAt:'2019-12-02'},
-            {id:6,nombre:'Laura',apellido:'Gonzalez',email:'laura@gmail.com',createAt:'2019-12-03'},
-            {id:7,nombre:'Natalia',apellido:'Echeverri',email:'natalia@gmail.com',createAt:'2019-12-05'}
-  ];
+  clientes: Cliente[];
 
 
   constructor() { }
 
+  ngOnInit() {
+   
+    this.clientes = CLIENTES;
+   
+  }
 
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,11 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id:1,nombre:'eduardo',apellido:'Rondón',email:'edu@gmail.com',createAt:'2019-12-04'},
+    {id:2,nombre:'jesus',apellido:'Fuentes',email:'jesus@gmail.com',createAt:'2019-12-03'},
+    {id:3,nombre:'Pedro',apellido:'Capriles',email:'pedro@gmail.com',createAt:'2019-12-02'},
+    {id:4,nombre:'Oscar',apellido:'Corvalan',email:'oscar@gmail.com',createAt:'2019-12-11'},
+    {id:5,nombre:'Hugo',apellido:'Rojas',email:'hugo@gmail.com',createAt:'2019-12-02'},
+    {id:6,nombre:'Laura',apellido:'Gonzalez',email:'laura@gmail.com',createAt:'2019-12-03'},
+    {id:7,nombre:'Natalia',apellido:'Echeverri',email:'natalia@gmail.com',createAt:'2019-12-05'}
+];
